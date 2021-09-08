28896eaf5d5a6a1ea60796a592f57ccfd4beecb7
CrS-21 <cristina.f.valverde@gmail.com>
2021-09-08 21:25:49 +0200

    add almost all issues fixed

--- js/main.js
+++ js/main.js
@@ -1,54 +1,50 @@
 'use strict';
 
-const randomNumber = getRandomNumber();
+const randomNumber = getRandomNumber(100);
 const tryButton = document.querySelector('.js_button');
 const userNumber = document.querySelector('.js_userNumber');
 const track = document.querySelector('.js_track');
 const attempt = document.querySelector('.js_attempt');
 let tryNumber = 0;
 
-
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * 100);
-    }
+  return Math.ceil(Math.random() * max);
+}
 
 console.log('Mi número aleatorio es ' + randomNumber);
 
+function writeMessage(sms) {
+  track.innerHTML = sms;
+}
 
-function takeNumber(){
+function takeNumber() {
+  const number = parseInt(userNumber.value);
 
-   const number = parseInt(userNumber.value);
-
-   if(number >0 && number <= 100){
-
-        if (number === randomNumber){
-           track.innerHTML = 'Has ganado campeona!!!'
-       }
-       else if (number < randomNumber){
-           track.innerHTML = 'Demasiado bajo.'
-       }
-       else if(number > randomNumber){
-           track.innerHTML = 'Demasiado alto.'
-       }
+  if (number > 0 && number <= 100) {
+    if (number === randomNumber) {
+      writeMessage('Has ganado campeona!!!');
+    } else if (number < randomNumber) {
+      writeMessage('Demasiado bajo.');
+    } else if (number > randomNumber) {
+      writeMessage('Demasiado alto.');
     }
-    else{
-        track.innerHTML = 'El número debe estar entre 1 y 100.'
-}
-}
-
-
-
-function takeCounter(){
-   tryNumber += +1;
-  attempt.innerHTML= 'Número de intentos ' + tryNumber; 
+  } else {
+    writeMessage('El número debe estar entre 1 y 100.');
+  }
 }
 
-function handleClick(){
+function takeCounter(ev) {
+  tryNumber += +1;
+  attempt.innerHTML = 'Número de intentos ' + tryNumber;
+}
+
+function handleClick() {
   takeNumber();
- takeCounter();
+  takeCounter();
 }
-tryButton.addEventListener('click' , handleClick);
+tryButton.addEventListener('click', handleClick);
 
-
-
-
+userNumber.addEventListener('submit', (ev) => {
+  ev.prevertDefault();
+  console.log(userNumber);
+});
